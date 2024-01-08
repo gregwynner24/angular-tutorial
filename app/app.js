2e94bad1fa35ec42ced55c007bd8ac9d21e44af4
Greg Wynner <gregwynner24@gmail.com>
2024-01-08 10:55:56 +0800

    First try upload existing angular project

---
+++ app/app.js
@@ -0,0 +1,87 @@
+var myNinjaApp = angular.module('myNinjaApp',['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+  .when('/home',{
+    templateUrl: 'view/home.html',
+    controller: 'NinjaController'
+  })
+  .when('/directory',{
+    templateUrl: 'view/directory.html',
+    controller: 'NinjaController'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  });
+
+}]);
+
+myNinjaApp.run(function(){
+
+});
+
+myNinjaApp.directive('randomNinja', [function(){
+
+return {
+  restrict: 'E', //E stands for Element and A stands for Attribute
+  scope: {
+    ninjas: '=',
+    title: '='
+  },
+  //template: '{{title}}',
+  //template: '<Img ng-src="{{ninjas[random].thumb}}">',
+  templateUrl: 'view/random.html',
+  transclude: true,
+  replace: false,
+  controller: function($scope){
+    $scope.random = Math.floor(Math.random() * 4);
+    
+  }
+};
+
+}]);
+
+myNinjaApp.controller('NinjaController',['$scope', '$http', function($scope, $http){
+
+  $scope.message = "hello from app.js";
+
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  };
+
+  $scope.addNinja = function(){
+    $scope.ninjas.push({
+      name:$scope.newninja.name,
+      belt: $scope.newninja.belt,
+      rate: parseInt($scope.newninja.rate),
+      available: true
+    });
+
+    $scope.newninja.name = "";
+    $scope.newninja.belt = "";
+    $scope.newninja.rate = "";
+  };
+
+  //0002 - THIS CODE IS UPDATED
+  $http.get('data/ninjas.json').then(function(response){
+    $scope.ninjas = response.data;
+  });
+  //0002 - END OF COMMENT
+
+  //0001 - THIS CODE IS OUTDATED
+  /*$http.get('data/ninjas.json').success(function(data){
+    $scope.ninjas = data;
+  });*/
+  //0001 - END OF COMMENT
+  
+  /*$scope.ninjas =[{
+    name: "Yoshi",
+    belt: "blue",
+    rate: 50,
+    available: true,
+    thumb: "src/img/cm.png"
+  }];*/
+
+}]);
